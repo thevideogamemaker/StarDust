9546c850c369a6932ac426bca6122f9cc131da9f
thevideogamemaker <adeouthman@Adeolas-MacBook-Pro.local>
2014-04-13 16:34:09 -0400

    Stardust

---
+++ Assets/Scripts/Enemy/Cyclops.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cyclops : MonoBehaviour {
+
+	public int HP = 5;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if(other.tag == "Star")
+		{
+			if(HP >= 1)
+			{
+				HP -=1;
+				Debug.Log("Enemy lost a life");
+			}
+		}
+	}
+}
